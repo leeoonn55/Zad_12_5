27c55d5cdb362d58a0b672d63ee0ed6b667e68e6
Leszek Macura <leszek@macura.co>
2018-10-08 12:27:55 +0200

    Zadanie 12_5 END

--- js/script.js
+++ js/script.js
@@ -33,18 +33,15 @@ function createTweet(input) {
         document.querySelector('.tweet').setAttribute('href', tweet);
     }
 
-    document.addEventListener('DOMContentLoaded', function() {
-        getQuote();
-        document.querySelector('.trigger').addEventListener('click', function() {
-            getQuote();
-        });
-    });
-
-
-
-
 
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    getQuote();
+    document.querySelector('.trigger').addEventListener('click', function() {
+        getQuote();
+    });
+});
 
 
 
